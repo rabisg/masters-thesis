78d1541f8487dfd414b913c19e7b905c87542234
rabisg <guha.rabishankar@gmail.com>
2015-06-12 20:09:31 +0530

    Complete chapter on implementation

--- hs/hindex.hs
+++ hs/hindex.hs
@@ -4,13 +4,12 @@ data HIndex doc p where
               hConfig         :: HIndexConfig
               -- | The in-memory terms of the index
             , hCurSegment     :: IORef (InMemorySegment doc b)
-              -- | Next segment number to write to
-            , hCurSegmentNum  :: IORef SegmentId
               -- | The term indices of the active segments
-              -- Currently the term indices of all active segments are stored
-              -- in memory. This provides a memory limitation to the implementation.
+              -- Currently the term indices of all active segments
+              -- are stored in memory
             , hActiveSegments :: IORef (Map SegmentId (Dictionary doc p))
-              -- | List of deleted document ids. Stored in memory and written
-              -- to disk with each flush operation
+              -- | List of deleted document ids.
+              -- Stored in memory and written to disk
+              -- with each flush operation
             , hDeletedDocs    :: IORef [DocumentId doc]
-            } -> HIndex a p
+            } -> HIndex doc p
