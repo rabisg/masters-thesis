5f42f8f2ea2f03e480532962a64aaa05c649d09d
rabisg <guha.rabishankar@gmail.com>
2015-06-16 13:30:07 +0530

    Make all programs haskell code

---
+++ hs/fibonacci.hs
@@ -0,0 +1,9 @@
+-- fib generates an infinite list of fibonacci numbers
+-- by zipping the list with itself
+fibs :: [Int]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+-- Now one can define a function to get the nth fibonacci number
+-- by just using the (!!) operator which fetches the nth Fibonacci number
+fibN :: Int -> Int
+fibN n = fibs !! n
